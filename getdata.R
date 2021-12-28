d157b3a668f4207cbc3465029e276eeba785d236
Nicholas Coxen <nicjcoxen@gmail.com>
2021-12-28 14:30:53 +1300

    plotly

--- getdata.R
+++ getdata.R
@@ -11,6 +11,7 @@ library(ggfortify)
 library(httr)
 library(plyr)
 library(readr)
+library(plotly)
 
 
 # Get the data 
@@ -259,6 +260,27 @@ df
 
 write.csv(data,"ufc.csv",row.names=FALSE)
 
-ggplot(data,aes(x= Height, fill = Stance )) + geom_bar()+ theme(axis.text.x = element_text(angle = 90))+ theme(axis.text.x = element_text(face = "bold", color = "black", size = 10),axis.text.y = element_text(face = "bold", color = "black", size = 10))+ scale_x_discrete(name="Height of fighter")+ scale_y_discrete(name="Count")
 
-ggsave("fighter height.png", width = 7, height = 7)
+ggplot(data,aes(x= Height, fill = Stance )) + geom_bar()+ theme(axis.text.x = element_text(angle = 90))+ theme(axis.text.x = element_text(face = "bold", color = "black", size = 10),axis.text.y = element_text(face = "bold", color = "black", size = 10))+ scale_x_discrete(name="Height of fighter")+ scale_y_discrete(name="Count", limits=c(0,20,40,60,80,100,120,140,160,180,200,220,240,260,280,300,320,340,360,380,400,420))+ labs(title="Count of Height by Fighter stance", subtitle="UFC data", caption="Fighter details")
+
+ggsave("fighter height.png", width = 6, height = 6)
+
+
+
+plt <- plot_ly(
+  x = data$Height,
+  y = data$Stance,
+  name = "Fighter Stance",
+  type = "bar"
+  
+)
+plt <- plt %>% layout(title = "Count of Height by Fighter stance and weight",xaxis = list(title = "Height"))
+plt <- plt %>% layout(yaxis = list(title = 'Count'), barmode = 'stack')
+plt <- plt %>% add_trace(y = ~data$Weight, name = 'Weight')
+plt <- plt %>% add_trace(y = ~data$Height, name = 'Height')
+
+
+plt
+
+ggsave("fighter height and stance.png", width = 6, height = 5)
+
